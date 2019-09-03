3b57585da1b893c0238f96a67c6ea10a6be15995
Justina Kuchta <justyna.kuchta@gmail.com>
2019-09-03 18:25:58 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        #takes in a string and adds that string to the student's @knowledge array
+        @knowledge << string
+    end
+
+    def knowledge
+        #returns that student's knowledge array
+        @knowledge
+    end
 end
